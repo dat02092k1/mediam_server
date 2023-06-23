b52c1d7ab1407e32210ccd96d9ad4f09ff8d520c
Dat Ngo <ngodat02092001@gmail.com>
2023-06-23 13:21:06 +0700

    "project config"

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const corOptions = require('./config/corsOptions');
+const connectDb = require('./config/dbConnect');
+const mongoose = require('mongoose');
+
+connectDb();
+app.use(cors(corOptions));
+app.use(express.json()); 
+app.use(cookieParser());
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+});
+
+mongoose.connection.on('error', err => {
+    console.log(err);
+})
